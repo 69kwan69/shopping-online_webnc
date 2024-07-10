9bcb468dc7a48a1cdf8d7d391104a8bea4227a16
69kwan69 <69hoangphi69@gmail.com>
2024-07-10 17:08:03 +0700

    Initial commit

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img
+          src="https://w7.pngwing.com/pngs/225/339/png-transparent-internet-meme-pol-anime-meme-child-mammal-hand.png"
+          width="800px"
+          alt=""
+        />
+      </div>
+    );
+  }
+}
+export default Home;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,13 @@
+// mongodb+srv://kwan_69:141107129@/
+const MyConstants = {
+  DB_SERVER: 'cluster0.r5kysjs.mongodb.net',
+  DB_USER: 'kwan_69',
+  DB_PASS: '141107129',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'KHONG_CO_KHOA',
+  JWT_EXPIRES: '341123123123123', // in milliseconds
+  EMAIL_USER: '69hoangphi69@gmail.com', // gmail service
+  EMAIL_PASS: 'fybkjlfgpfqgaabp',
+};
+
+module.exports = MyConstants;
